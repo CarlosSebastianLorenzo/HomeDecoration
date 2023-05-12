bf7eac560acca278d0c895687b6d7cecc4985103
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-05-11 21:04:12 -0300

    slider

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
+
+
 document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide' );
+    var splide = new Splide( '.splide', {
+        type : 'loop',
+        drag : 'free',
+        snap : true,
+        perPage: 1,
+        perMove: 1,
+    } );
     splide.mount();
 } );
