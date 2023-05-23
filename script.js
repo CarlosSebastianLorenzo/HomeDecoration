8a81b5cf0154bddbd01a2f174ac8444ca219b92e
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-05-23 00:09:48 -0300

    button up, autoplay carrousel, hovers

--- script.js
+++ script.js
@@ -2,12 +2,30 @@
 
 
 document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide', {
+    const splide = new Splide( '.splide', {
         type : 'loop',
         drag : 'free',
         snap : true,
+        autoplay: true,
+        interval: 3000,
         perPage: 1,
         perMove: 1,
     } );
     splide.mount();
 } );
+
+// SCROLL BUTTON TO TOP
+const scrollButton = document.getElementById("scrollButton");
+
+function scrollToTop() {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}
+
+// HIDE BUTTON
+function scrollFunction() {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    scrollButton.classList.toggle("show", scrollTop > 20);
+}
+
+window.addEventListener("scroll", scrollFunction);
